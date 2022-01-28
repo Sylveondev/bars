3a4427b0453806a8a7e1519aa65d0305ef0701c8
Spongey <spongebob7989a@gmail.com>
2022-01-28 01:08:06 +0000

    Add some comments and filtered out some more secrets

--- events/ready.js
+++ events/ready.js
@@ -2,12 +2,19 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		require('../autoicon').run(client);
+		
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 		var ms = require('ms');
 
-		setInterval(async ()=>{var g = await client.guilds.fetch('891539642246123530')
+		// This is specifically for my disord server, since I retired spongeybot and Spongey MEE6. 
+		// Your bot won't execute these lines.
+		// I will not provide any help in getting this to work.
+		// You're on your own in figuring out how to run this.
+		if (client.user.id = "891539642246123530"){
+			require('../autoicon').run(client);
+			setInterval(async ()=>{var g = await client.guilds.fetch('891539642246123530')
 		
-		g.roles.edit('899513337484828744',{color:"RANDOM"},'Random color role change')},ms('5m'))
+			g.roles.edit('899513337484828744',{color:"RANDOM"},'Random color role change')},ms('5m'))
+		}
 	},
 };
